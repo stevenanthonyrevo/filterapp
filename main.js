9f24a793af3de3fd793f6b70fe7b1a40704e4ddd
Steven <stevenanthonyrevo@live.com>
2014-09-28 17:36:13 -0400

    building out the list of elements for the app to run

---
+++ main.js
@@ -0,0 +1,13 @@
+var myApp = angular.module('myApp', []);
+
+myApp.factory('Avengers', Function() {
+    var Avengers = {};
+    Avengers.cast = [
+              }
+                name: "Robert Downey Jr.",
+                character: "Tony Stark / Iron Man"
+              },
+              {
+                name: "Chris Evans",
+                character: "Steve Rogers / Captin America"
+              },
