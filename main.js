db1184b43bb6d4376a53c7750247b33ed09faf23
Steven <stevenanthonyrevo@live.com>
2014-09-28 18:54:11 -0400

    finally got all of the working parts working

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 var myApp = angular.module('myApp', []);
 
-myApp.factory('Avengers', Function() {
+myApp.factory('Avengers', function() {
     var Avengers = {};
     Avengers.cast = [
-              }
-                name: "Robert Downey Jr.",
-                character: "Tony Stark / Iron Man"
-              },
-              {
-                name: "Chris Evans",
-                character: "Steve Rogers / Captin America"
-              },
+     {
+      name: "Robert Downey Jr.",
+      character: "Tony Stark / Iron Man"
+        },
+        {
+           name: "Chris Evans",
+           character: "Steve Rogers / Captin America"
+        },
+        {
+        name: "Mark Ruffalo",
+        character: "Bruce Banner / The Hulk"
+      },
+    {
+        name: "Tina Benko",
+        character: "NASA Scientist"
+      }
+    ];
+    return Avengers;
+})
+
+function AvengersCtrl($scope, Avengers) {
+    $scope.avengers = Avengers;
+}
